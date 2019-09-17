133be8b058d666b89d42def0305e221d9ee669c4
Tom Belcher <tbelcher@verizon.net>
2019-09-17 13:48:05 -0400

    Don't start a snapshot if one is pending

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages ec2, volumes and snapshots"""
@@ -99,6 +103,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
         icount += 1
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("   Skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("Creating snapshot for {0}".format(v.id))
             v.create_snapshot(Description="Created by Snapshotalyzer 30000")
             vcount += 1
